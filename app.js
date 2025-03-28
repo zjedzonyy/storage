19f3bc59f99268ad89d70d0e127b6191a788b774
zjedzonyy <artompor@gmail.com>
2025-03-28 12:02:22 +0100

    feat(db): add initial database schema

---
+++ app.js
@@ -0,0 +1,35 @@
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.listen("3000", () =>
+  console.log("Server started listening at port 3000...")
+);
